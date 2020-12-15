1ec5917beffe916339bfb0a90d28954da30efac7
benm_oc <ben.murray@ocado.com>
2020-12-15 23:23:54 +0000

    day 16 files prepped

---
+++ day16/src/com/aoc/benjm/DayN.java
@@ -0,0 +1,18 @@
+package com.aoc.benjm;
+
+import java.util.Scanner;
+
+
+public class DayN {
+    public long partOne(String filename) {
+        Scanner scanner = new Scanner(DayN.class.getResourceAsStream(filename));
+        //TODO
+        return 0l;
+    }
+
+    public long partTwo(String filename) {
+        Scanner scanner = new Scanner(DayN.class.getResourceAsStream(filename));
+        //TODO
+        return 0l;
+    }
+}
