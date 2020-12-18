cdd9698af2524edc46dada914b458233ca322fb3
benjm <benjmurray@gmail.com>
2020-12-18 22:32:28 +0000

    day 18 completed

--- day18/src/com/aoc/benjm/DayN.java
+++ day18/src/com/aoc/benjm/DayN.java
@@ -1,5 +1,8 @@
 package com.aoc.benjm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class DayN {
@@ -13,7 +16,102 @@ public class DayN {
 
     private long run(String filename, boolean isPartTwo) {
         Scanner scanner = new Scanner(DayN.class.getResourceAsStream(filename));
-        //TODO
-        return 0l;
+        long sumTotal = 0l;
+        while(scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String tokenArray[] = line.replace("(","( ").replace(")"," )").split(" ");
+            List<String> tokens;
+            tokens = Arrays.asList(tokenArray);
+            sumTotal += eval(tokens, isPartTwo);
+        }
+        return sumTotal;
+    }
+
+    private long eval(List<String> tokens, boolean isPartTwo) {
+        if (tokens.size() == 1) return Integer.parseInt(tokens.get(0));
+        int bracketCount = 0;
+        List<String> subBracket = new ArrayList<>();
+        List<String> bracketsEvaluated = new ArrayList<>();
+        for(String token : tokens) {
+            if(token.equals("(")) {
+                if (bracketCount > 0) {
+                    subBracket.add(token);
+                }
+                bracketCount++;
+            } else if(token.equals(")")) {
+                bracketCount--;
+                if (bracketCount > 0) {
+                    subBracket.add(token);
+                } else {
+                    bracketsEvaluated.add(String.valueOf(eval(subBracket, isPartTwo)));
+                    subBracket.clear();
+                }
+            } else if (bracketCount > 0) {
+                subBracket.add(token);
+            } else {
+                bracketsEvaluated.add(token);
+            }
+        }
+        // evaluation left to right
+        // if (isPartTwo) do + before *
+        if (isPartTwo) return evalPartTwoFinal(bracketsEvaluated);
+        int index = 0;
+        Long result = Long.valueOf(bracketsEvaluated.get(index++));
+        while (index < bracketsEvaluated.size() - 1) {
+            String function = bracketsEvaluated.get(index++);
+            Long next = Long.parseLong(bracketsEvaluated.get(index++));
+            if (function.equals("+")) {
+                result += next;
+            } else if (function.equals("*")) {
+                result *= next;
+            } else {
+                throw new RuntimeException("oops ... " + function + " is not a function...?");
+            }
+        }
+        return result;
+    }
+
+    private long evalPartTwoFinal(List<String> tokens) {
+        List<String> noPluses = new ArrayList<>();
+        if (tokens.contains("+")) {
+            int index = 0;
+            String left = tokens.get(0);
+            for (int i = 0; i < tokens.size() - 2; i+=2) {
+                String function = tokens.get(i+1);
+                String right = tokens.get(i+2);
+                if (function.equals("+")) {
+                    Long plusResult = Long.parseLong(left) + Long.parseLong(right);
+                    left = String.valueOf(plusResult);
+                    if (i+2 == tokens.size() - 1) {
+                        noPluses.add(left);
+                    }
+                } else {
+                    // copy i and i+1
+                    noPluses.add(left);
+                    noPluses.add(function);
+                    if (i+2 == tokens.size() - 1) {
+                        noPluses.add(right);
+                    }
+                    left = right;
+                }
+            }
+        } else {
+            noPluses = tokens;
+        }
+
+        int index = 0;
+        Long result = Long.valueOf(noPluses.get(index++));
+        while (index < noPluses.size() - 1) {
+            String function = noPluses.get(index++);
+            Long next = Long.parseLong(noPluses.get(index++));
+            if (function.equals("+")) {
+                result += next;
+            } else if (function.equals("*")) {
+                result *= next;
+            } else {
+                throw new RuntimeException("oops ... " + function + " is not a function...?");
+            }
+        }
+        return result;
     }
 }
--- day18/src/com/aoc/benjm/DayNTest.java
+++ day18/src/com/aoc/benjm/DayNTest.java
@@ -6,12 +6,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DayNTest {
 
+    /*
+    1 + 2 * 3 + 4 * 5 + 6 becomes 71.
+    1 + (2 * 3) + (4 * (5 + 6)) becomes 51.
+    2 * 3 + (4 * 5) becomes 26.
+    5 + (8 * 3 + 9 + 3 * 4 * 3) becomes 437.
+    5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4)) becomes 12240.
+    ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2 becomes 13632.
+     */
     @Test
     void partOneExample() {
         long start = System.currentTimeMillis();
         Long l = new DayN().partOne("/testInput.txt");
         log("outp: " + l + "\ntime : " + (System.currentTimeMillis() - start));
-        assertEquals(-1l, l);
+        assertEquals(26457l, l);
     }
 
     @Test
@@ -19,7 +27,7 @@ class DayNTest {
         long start = System.currentTimeMillis();
         Long l = new DayN().partOne("/input.txt");
         log("outp: " + l + "\ntime : " + (System.currentTimeMillis() - start));
-        assertEquals(-1l, l);
+        assertEquals(36382392389406l, l);
     }
 
     @Test
@@ -27,7 +35,7 @@ class DayNTest {
         long start = System.currentTimeMillis();
         Long l = new DayN().partTwo("/testInput.txt");
         log("outp: " + l + "\ntime : " + (System.currentTimeMillis() - start));
-        assertEquals(-1l, l);
+        assertEquals(694173l, l);
     }
 
     @Test
@@ -35,7 +43,7 @@ class DayNTest {
         long start = System.currentTimeMillis();
         Long l = new DayN().partTwo("/input.txt");
         log("outp: " + l + "\ntime : " + (System.currentTimeMillis() - start));
-        assertEquals(-1l, l);
+        assertEquals(381107029777968l, l);
     }
 
     private static void log(final Object o) {
